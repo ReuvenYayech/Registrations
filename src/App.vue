868e0c0649245c2a97c63b605247c00a698215a9
reuven <reuveny@activetrail.com>
2018-12-26 17:27:04 +0200

    asdasd

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <app-registration></app-registration>
+    <registrations></Registrations>
+  </div>
+</template>
+
+<script>
+  import Registration from './components/Registration.vue';
+  import Registrations from './components/Registrations.vue';
+
+export default {
+    components: {
+        appRegistration: Registration,
+        Registrations: Registrations
+    }
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
